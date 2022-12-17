877014756052639f150a8ccfef04044f18a3e4d7
shivxD1 <69270090+shivxD1@users.noreply.github.com>
2022-12-17 16:55:49 +0000

    removed creds

--- server/controller/mailer.js
+++ server/controller/mailer.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const transport = nodemailer.createTransport({
     service: 'Mailgun',
     auth: {
-        user: "postmaster@sandbox158b67c13cc1442b86fd605b7fc48f7a.mailgun.org",
-        pass: 'e9a5ed1b76ae3f32eeee5331f3a696aa-48d7d97c-f6c3e78c'
+        user: "USE YOUR MAILGUN CREDENTIALS HERE",
+        pass: 'USE YOUR MAILGUN CREDENTIALS HERE'
     },
     secure: false,
     port: 25,
